b9437f907adff39a7a2b1def46c29c348fcba188
smile1218 <supitcha.sawasd@hotmail.com>
2016-09-13 16:40:05 +0700

    580610684

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -8,21 +8,33 @@ public class User {
     public String password;
 
     public User() {
+        setUserName("");
+        setPassword("");
     }
 
     public boolean setUserName(String name) {
+        String A = "[a-zA-Z][A-Za-z0-9]{7,}";
+        if(name.matches(A)){
+            userName = name;
+            return true;
+        }
         return false;
     }
 
     public boolean setPassword(String name) {
+        String B = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{12,}";
+        if (name.matches(B)){
+            password = name;
+            return true;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
