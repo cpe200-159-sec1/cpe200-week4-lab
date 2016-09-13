44abcdc8a3c3deac1c4a48e0b8e85bd98297d686
ขี้เอง <ping31345@gmail.com>
2016-09-13 16:06:36 +0700

    Lab1 fin

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,65 @@
 package cpe200;
 
+import java.math.BigDecimal;
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private String firstOperand, secondOperand;
 
-    public BinaryCalculator() {
+    public BinaryCalculator()
+    {
     }
 
-    public void setFirstOperand(Operand operand) {
+    public void setFirstOperand(Operand operand)
+    {
+        this.firstOperand = operand.get();
     }
 
 
-    public void setSecondOperand(Operand operand) {
+    public void setSecondOperand(Operand operand)
+    {
+
+        this.secondOperand = operand.get();
     }
 
-    public String add() {
-        return null;
+    public String add()
+    {
+        BigDecimal a = new BigDecimal(this.firstOperand);
+        BigDecimal b = a.add(new BigDecimal(this.secondOperand)).stripTrailingZeros();
+        return b.toString();
     }
 
-    public String subtract() {
-        return null;
+    public String subtract()
+    {
+        BigDecimal a = new BigDecimal(this.firstOperand);
+        BigDecimal b = a.subtract(new BigDecimal(this.secondOperand)).stripTrailingZeros();
+        return b.toString();
     }
 
-    public String multiply() {
-        return null;
+    public String multiply()
+    {
+        BigDecimal a = new BigDecimal(this.firstOperand);
+        BigDecimal b = a.multiply(new BigDecimal(this.secondOperand)).stripTrailingZeros();
+        return b.toString();
     }
 
     /* This method should throw an exception when divide by zero */
-    public String division() throws ArithmeticException {
-        return null;
+    public String division()
+    {
+        BigDecimal a = new BigDecimal(this.firstOperand);
+        BigDecimal b = new BigDecimal(this.secondOperand);
+        if (b.compareTo(BigDecimal.ZERO) == 0)
+            throw new IllegalArgumentException("Argument 'divisor' is 0");
+        b = a.divide(b, 5, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+
+        return b.toString();
     }
 
     public String power() {
-        return null;
+        BigDecimal a = new BigDecimal(this.firstOperand);
+        BigDecimal b = new BigDecimal(this.secondOperand);
+        a = a.pow(b.intValue());
+        a = a.stripTrailingZeros();
+        return a.toString();
     }
 
 }
