f574d50ec6b0942b3b8e6fe2780ab89cc130986d
Aunpyz <aunnop_kattiyanet@hotmail.com>
2016-09-13 18:39:38 +0700

    580610689

--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -6,44 +6,64 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    private ArrayList<User> userList;
 
+    //Constructor of Users
+    public Users()
+    {
+        userList = new ArrayList<>();
+    }
     public void addUser(User user)
     {
+        userList.add(user);
     }
 
     public void addUser(String userName, String password)
     {
+        User newUser = new User(userName, password);
+        userList.add(newUser);
     }
 
     public void deleteUser(User user)
     {
-
+        userList.remove(user);
     }
 
     public boolean exists(User user)
     {
-        return false;
+        return userList.contains(user);
     }
 
     public boolean usernameExists(String username)
     {
+        for(User user: userList)
+        {
+            if(user.getUserName().equals(username))
+            {
+                return true;
+            }
+        }
         return false;
     }
 
     /* This method should return null when the user with username is not in the list */
     public User getUserByUsername(String userName)
     {
+        for(User user: userList)
+        {
+            if(user.getUserName().equals(userName))
+                return user;
+        }
         return null;
     }
 
     public int count()
     {
-        return 0;
+        return userList.size();
     }
 
     public User[] getUserArray()
     {
-        return null;
+        return userList.toArray(new User[count()]);
     }
 }
