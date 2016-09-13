f5de895e79f7fb76be056cfcef1e4b5761f0ce4e
Potsawat <potsawat_3232@hotmail.com>
2016-09-13 14:23:44 +0700

    lab 4-1 Done

--- lab4_1/src/cpe200Test/BinaryCalculatorTest.java
+++ lab4_1/src/cpe200Test/BinaryCalculatorTest.java
@@ -5,8 +5,8 @@ import cpe200.Operand;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import java.lang.reflect.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import static org.junit.Assert.*;
 
@@ -197,7 +197,7 @@ public class BinaryCalculatorTest {
         secondOperand = new Operand(2.0);
         binaryCalculator.setFirstOperand(firstOperand);
         binaryCalculator.setSecondOperand(secondOperand);
-        assertEquals("8", binaryCalculator.multiply());
+        assertEquals("9", binaryCalculator.power());
     }
 
     @Test
