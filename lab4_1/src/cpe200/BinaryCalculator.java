27384c900007daf83844ce5075723d74126783ba
icqcq39 <cute_cloudian@hotmail.com>
2016-09-13 20:12:22 +0700

    OK DONE

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -18,24 +18,38 @@ public class BinaryCalculator {
     }
 
     public String add() {
-        return Double.toString(Double.parseDouble(firstOperand.getOperand()) + Double.parseDouble(secondOperand.getOperand())).replaceAll(".0","");
+        String ans = String.format("%."+ 5 + "f", Double.parseDouble(firstOperand.getOperand()) + Double.parseDouble(secondOperand.getOperand()));
+        ans = ans.indexOf(".") < 0 ? ans : ans.replaceAll("0*$", "").replaceAll("\\.$", "");
+        return ans;
     }
 
     public String subtract() {
-        return null;
+        String ans = String.format("%."+ 5 + "f", Double.parseDouble(firstOperand.getOperand()) - Double.parseDouble(secondOperand.getOperand()));
+        ans = ans.indexOf(".") < 0 ? ans : ans.replaceAll("0*$", "").replaceAll("\\.$", "");
+        return ans;
     }
 
     public String multiply() {
-        return null;
+        String ans = String.format("%."+ 5 + "f", Double.parseDouble(firstOperand.getOperand()) * Double.parseDouble(secondOperand.getOperand()));
+        ans = ans.indexOf(".") < 0 ? ans : ans.replaceAll("0*$", "").replaceAll("\\.$", "");
+        return ans;
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        if (secondOperand.getOperand().equals("0") || secondOperand.getOperand().equals("0.0")){
+            throw new ArithmeticException("Divide by zero");
+        }else {
+            String ans = String.format("%." + 5 + "f", Double.parseDouble(firstOperand.getOperand()) / Double.parseDouble(secondOperand.getOperand()));
+            ans = ans.indexOf(".") < 0 ? ans : ans.replaceAll("0*$", "").replaceAll("\\.$", "");
+            return ans;
+        }
     }
 
     public String power() {
-        return null;
+        String ans = String.format("%."+ 5 + "f", Math.pow( Double.parseDouble(firstOperand.getOperand()) , Double.parseDouble(secondOperand.getOperand())));
+        ans = ans.indexOf(".") < 0 ? ans : ans.replaceAll("0*$", "").replaceAll("\\.$", "");
+        return ans;
     }
 
 }
