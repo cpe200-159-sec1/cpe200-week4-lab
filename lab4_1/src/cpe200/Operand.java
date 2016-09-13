d38db54aa4836cbe741ca7595fcdccc683a61fae
Pimukthee\wobwab <pimukthee@gmail.com>
2016-09-13 14:14:38 +0700

    580610662 lab4 Done

--- lab4_1/src/cpe200/Operand.java
+++ lab4_1/src/cpe200/Operand.java
@@ -1,18 +1,24 @@
 package cpe200;
 
 public class Operand {
-    public String operand;
+    private String operand;
 
     public Operand(String operand) {
         this.operand = operand;
     }
 
-    public Operand(int operand) {
-
+    public Operand(int operand)
+    {
+        this.operand = Integer.toString(operand);
     }
 
-    public Operand(double operand) {
-
+    public Operand(double operand)
+    {
+        this.operand = String.valueOf(operand);
+    }
+    public String getoperand()
+    {
+        return operand;
     }
 
 }
