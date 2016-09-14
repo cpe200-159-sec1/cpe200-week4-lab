1dc98b98fddb984882e1e82c05b37f32d028f822
Wiphob <wiphobkaoian@gmail.com>
2016-09-14 18:30:27 +0700

    580610677 Finished Lab 4-1 Binary

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,59 @@
 package cpe200;
-
+import java.math.BigDecimal;
 
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
-
+    private Operand firstOperand;
+    private Operand secondOperand;
+    private BigDecimal FOP;
+    private BigDecimal SOP;
     public BinaryCalculator() {
+
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand;
+        FOP = new BigDecimal(firstOperand.getOperand());
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand;
+        SOP = new BigDecimal(secondOperand.getOperand());
     }
 
     public String add() {
-        return null;
+
+        return FOP.add(SOP).stripTrailingZeros().toString();
     }
 
     public String subtract() {
-        return null;
+
+        return FOP.subtract(SOP).stripTrailingZeros().toString();
     }
 
     public String multiply() {
-        return null;
+
+        return FOP.multiply(SOP).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+
+        if(SOP.equals(0)) {
+            throw new ArithmeticException("ERROR");
+        }
+        return FOP.divide(SOP,5,BigDecimal.ROUND_UP).stripTrailingZeros().toString();
     }
 
     public String power() {
-        return null;
+
+        return FOP.pow(SOP.intValue()).stripTrailingZeros().toString();
+        }
+
+    public Operand getFirstOperand() {
+        return firstOperand;
     }
 
+    public Operand getSecondOperand() {
+        return secondOperand;
+    }
 }
