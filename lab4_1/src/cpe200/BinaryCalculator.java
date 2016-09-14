a178163dfaedfab5270fcaddbeb3b3c78a412304
KaninSukarote <kanin.sukarote@gmail.com>
2016-09-14 15:29:32 +0700

    580610622

--- lab4_1/src/cpe200/BinaryCalculator.java
+++ lab4_1/src/cpe200/BinaryCalculator.java
@@ -1,39 +1,58 @@
 package cpe200;
 
 
+import java.math.BigDecimal;
+
 public class BinaryCalculator {
-    public Operand firstOperand;
-    public Operand secondOperand;
+    private Operand firstOperand;
+    private Operand secondOperand;
 
     public BinaryCalculator() {
+        firstOperand = new Operand(0);
+        secondOperand = new Operand(0);
+
     }
 
     public void setFirstOperand(Operand operand) {
+        firstOperand = operand ;
     }
 
-
     public void setSecondOperand(Operand operand) {
+        secondOperand = operand ;
     }
 
     public String add() {
-        return null;
+        BigDecimal a = new BigDecimal(firstOperand.getOperand());
+        BigDecimal b = new BigDecimal(secondOperand.getOperand());
+
+        return a.add(b).stripTrailingZeros().toString();
     }
 
     public String subtract() {
-        return null;
+        BigDecimal a = new BigDecimal(firstOperand.getOperand());
+        BigDecimal b = new BigDecimal(secondOperand.getOperand());
+        return a.subtract(b).stripTrailingZeros().toString();
     }
 
     public String multiply() {
-        return null;
+        BigDecimal a = new BigDecimal(firstOperand.getOperand());
+        BigDecimal b = new BigDecimal(secondOperand.getOperand());
+
+        return a.multiply(b).stripTrailingZeros().toString();
     }
 
     /* This method should throw an exception when divide by zero */
     public String division() throws ArithmeticException {
-        return null;
+        BigDecimal a = new BigDecimal(firstOperand.getOperand());
+        BigDecimal b = new BigDecimal(secondOperand.getOperand());
+        return a.divide(b,5,BigDecimal.ROUND_HALF_UP).stripTrailingZeros().toString();
     }
 
     public String power() {
-        return null;
+
+        BigDecimal a = new BigDecimal(firstOperand.getOperand());
+        BigDecimal b = new BigDecimal(secondOperand.getOperand());
+        return a.pow(b.intValue()).stripTrailingZeros().toString();
     }
 
 }
--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,39 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+
     }
 
     public boolean setUserName(String name) {
-        return false;
+        String condition = "[a-zA-Z][a-zA-Z0-9]{7,}";
+        if(name.matches(condition)){
+            this.userName = name;
+            return true;
+        }else
+            return false ;
     }
 
     public boolean setPassword(String name) {
-        return false;
+
+        String condition = "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{12,}";
+        if(name.matches(condition)){
+            this.password = name;
+            return true;
+        }else
+            return false ;
     }
 
-    public String getUserName() {
-        return null;
+    public String getUserName()
+    {
+        return userName;
     }
 
-    public String getPassword() {
-        return null;
+    public String getPassword()
+    {
+        return password;
     }
 }
