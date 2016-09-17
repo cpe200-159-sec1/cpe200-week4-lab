44bd5a8ba8bdc2845d925e0b1f7e1eb465f064e9
phusitkrittikarak <sutamoo@hotmail.com>
2016-09-18 03:18:08 +0700

    580610669 Lab4 Fin

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -4,25 +4,39 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName;
+    private String password;
 
     public User() {
+        userName = "";
+        password = "";
     }
 
     public boolean setUserName(String name) {
+        String correct="^[a-zA-Z][a-zA-Z0-9]{7,}$";
+        if(name.matches(correct))
+        {
+            userName = name;
+            return true;
+        }
         return false;
     }
 
     public boolean setPassword(String name) {
+        String correct="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-1])[A-Za-z0-9]{12,}$";
+        if(name.matches(correct))
+        {
+            password = name;
+            return true;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+        return this.userName;
     }
 
     public String getPassword() {
-        return null;
+        return this.password;
     }
 }
