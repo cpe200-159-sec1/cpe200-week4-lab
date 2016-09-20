9a82018ae02d3e59ca442c8969ad9be9170f4042
sharof <pro21808@gmail.com>
2016-09-20 15:34:45 +0700

    580610659

--- lab4_2/src/cpe200/Users.java
+++ lab4_2/src/cpe200/Users.java
@@ -6,44 +6,51 @@ import java.util.ArrayList;
  * Created by pruet on 12/9/2559.
  */
 public class Users {
-    public ArrayList<User> userList;
+    public ArrayList<User> userList = new ArrayList<User>();
 
-    public void addUser(User user)
-    {
+    public void addUser(User user) {
+        userList.add(user);
     }
 
-    public void addUser(String userName, String password)
-    {
+    public void addUser(String userName, String password) {
+        userList.add(new User(userName,password));
     }
 
-    public void deleteUser(User user)
-    {
-
+    public void deleteUser(User user) {
+        userList.remove(user);
     }
 
-    public boolean exists(User user)
-    {
+    public boolean exists(User user) {
+        if(userList.contains(user)){
+            return true;
+        }
         return false;
     }
 
-    public boolean usernameExists(String username)
-    {
-        return false;
+    public boolean usernameExists(String username) {
+        for(int i=0;i<userList.size();i++){
+            if(userList.get(i).getUserName().equalsIgnoreCase(username)){
+                return true;
+            }
+        }
+        return  false;
     }
 
     /* This method should return null when the user with username is not in the list */
-    public User getUserByUsername(String userName)
-    {
-        return null;
+    public User getUserByUsername(String userName) {
+        for(int i=0;i<userList.size();i++){
+            if(userList.get(i).getUserName().equalsIgnoreCase(userName)){
+                return  userList.get(i);
+            }
+        }
+        return  null;
     }
 
-    public int count()
-    {
-        return 0;
+    public int count() {
+        return  userList.size();
     }
 
-    public User[] getUserArray()
-    {
-        return null;
+    public User[] getUserArray() {
+        return userList.toArray(new User[count()]);
     }
 }
